4e83aba5d244fd3916346d08365dc53297ae1648
Uzayr <uzayr.wani.23@ucl.ac.uk>
2025-10-10 17:12:27 +0100

    203 activity

--- src/activities/my_solutions/solutions.py
+++ src/activities/my_solutions/solutions.py
@@ -1,5 +1,6 @@
 def main():
-    tutorial_202()
+    csv_df, xlsx_df1, xlsx_df2 =tutorial_202()
+    tutorial_203(csv_df)
 
 def tutorial_201():
     from pathlib import Path
@@ -31,12 +32,34 @@ def tutorial_202():
     csv_file = solutions_root_root.joinpath("data", "paralympics_raw.csv")
     xlsx_file = solutions_root_root.joinpath("data", "paralympics_all_raw.xlsx")
 
+    #converts to dataframes, does it for each sheet in xlsx
     csv_df = pd.read_csv(csv_file)
-    pd.read_excel(xlsx_file, sheet_name=0)
-    pd.read_excel(xlsx_file, sheet_name=1)
+    xlsx_df1 = pd.read_excel(xlsx_file, sheet_name=0)
+    xlsx_df2 = pd.read_excel(xlsx_file, sheet_name=1)
 
-def tutorial_203():
+    return(csv_df, xlsx_df1, xlsx_df2)
+
+def tutorial_203(dataframe):
+    '''Summary of description of function here
     
+    Parameters:
+    dataframe (dataframe): dataframe
+    
+    Returns:
+    str: Description of the dataframes
+    '''
+
+    import pandas as pd
+
+    print(dataframe.shape)
+    pd.set_option("display.max_columns", None)
+    print(dataframe.head)
+    print(dataframe.tail)
+    print(dataframe.columns)
+    print(dataframe.dtypes)
+    print(dataframe.info)
+    print(dataframe.describe)
+
 
 if __name__ == "__main__":
     main()
